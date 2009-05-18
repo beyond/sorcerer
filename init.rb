e19ab5e70c6f801256484db7b36b91f5b876afe8
beyond <beyond@be.to>
2009-05-18 15:58:40 +0900

    delete WizardPage

--- init.rb
+++ init.rb
@@ -2,9 +2,9 @@ require File.expand_path(File.join(File.dirname(__FILE__), *%w[lib acts_as_wizar
 
 ActiveRecord::Base.class_eval do
   include AmosKing::Acts::Wizard
-	include AmosKing::Acts::WizardPage
+# include AmosKing::Acts::WizardPage
 end
 
 ActionView::Base.class_eval do
-	include AmosKing::Acts::WizardHelper
+  include AmosKing::Acts::WizardHelper
 end
